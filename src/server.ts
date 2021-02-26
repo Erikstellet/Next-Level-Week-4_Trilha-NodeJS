b963f801b772a7768ad3b34fd79007ceb0bb076e
Erikstellet <erikstellet@gmail.com>
2021-02-26 09:28:15 -0300

    NLW#4 | Trilha NodeJS - Aula 2

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,10 @@
+import 'reflect-metadata'
 import express from 'express';
+import './database';
+import { router } from '../routes';
 
 const app = express();
 
-/*
-    GET    =>   Busca
-    POST   =>   Salvar
-    PUT    =>   Alterar
-    DELETE =>   Deleter
-    PATCH  =>   Alteração Específica             
-*/
-
-// http://localhost:3333/users
-app.get("/", (request, response) => 
-{
-   // return response.send("Hello World! - NLW04")
-   return response.json({ message: "Hello World! - NLW04"}) 
-})
-
-// 1 param => Rota(Recurso API)
-// 2 param => request.response
-app.post("/", (request, response) => 
-{
-    // Recebeu os dados para salvar
-    return response.json({ message: "Os dados foram salvos com sucesso"})
-})
-
+app.use(express.json());
+app.use(router);
 app.listen(3333, () => console.log("Server Open!"))
